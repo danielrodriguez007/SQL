5a371d6b2d41548c00ea3fcbc22ed3dd336a26a5
Daniel Rodriguez <71362610+danielrodriguez007@users.noreply.github.com>
2023-01-05 12:49:15 -0500

    Update Employers_Management.sql

--- Employers_Management.sql
+++ Employers_Management.sql
@@ -49,3 +49,61 @@ SELECT * FROM empleado;
 
 SELECT * FROM empleado
 WHERE id = 1;-- 1.Lista el primer apellido de todos los empleados.
+
+SELECT apellido1,COUNT(apellido1) as cantApellido FROM empleado
+WHERE id = 1
+GROUP By apellido1
+HAVING cantApellido = 1;-- 2. Lista el primer apellido de los empleados eliminando los apellidos que estén repetidos.
+
+SELECT * FROM empleado;-- 3.Lista todas las columnas de la tabla empleado.
+
+SELECT nombre, apellido1, apellido2 FROM empleado;-- 4.Lista el nombre y los apellidos de todos los empleados.
+
+SELECT departamento.nombre FROM departamento
+LEFT JOIN empleado
+ON departamento.id = empleado.id_departamento;-- 5.Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado.
+
+SELECT DISTINCT(departamento.nombre) FROM departamento
+LEFT JOIN empleado
+ON departamento.id=empleado.id_departamento;-- 6.Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado, eliminando los identificadores que aparecen repetidos
+
+SELECT CONCAT(COALESCE(nombre, ''),COALESCE(apellido1, ''),COALESCE(apellido2, '')) AS nombreCompleto FROM empleado;-- 7.Lista el nombre y apellidos de los empleados en una única columna.
+
+SELECT UPPER(CONCAT(COALESCE(nombre, ''), COALESCE(apellido1, ''), COALESCE(apellido2, ''))) AS nombreCompleto FROM empleado;-- 8.Lista el nombre y apellidos de los empleados en una única columna, convirtiendo todos los caracteres en mayúscula.
+
+SELECT LOWER(CONCAT(COALESCE(nombre, ''), COALESCE(apellido1, ''), COALESCE(apellido2, ''))) AS nombreCompleto FROM empleado;-- 9.Lista el nombre y apellidos de los empleados en una única columna, convirtiendo todos los caracteres en minúscula.
+
+SELECT id, REGEXP_SUBSTR(nif, '^[0-9]+') AS nifNumbers, REGEXP_SUBSTR(nif,'[A-Z]') AS nifLetters FROM empleado;-- 10.Lista el identificador de los empleados junto al nif, pero el nif deberá aparecer en dos columnas, una mostrará únicamente los dígitos del nif y la otra la letra.
+
+SELECT CONCAT(presupuesto-gastos) AS presupuestoActual FROM departamento;-- 11.Lista el nombre de cada departamento y el valor del presupuesto actual del que dispone. Para calcular este dato tendrá que restar al valor del presupuesto inicial (columna presupuesto) los gastos que se han generado (columna gastos). Tenga en cuenta que en algunos casos pueden existir valores negativos. Utilice un alias apropiado para la nueva columna columna que está calculando.
+
+SELECT nombre, CONCAT(presupuesto-gastos) AS presupuestoActual FROM departamento
+ORDER BY presupuestoActual ASC;-- 12.Lista el nombre de los departamentos y el valor del presupuesto actual ordenado de forma ascendente.
+
+SELECT nombre FROM departamento
+ORDER BY nombre ASC;-- 13.Lista el nombre de todos los departamentos ordenados de forma ascendente.
+
+SELECT nombre FROM departamento
+ORDER BY nombre DESC;-- 14.Lista el nombre de todos los departamentos ordenados de forma desscendente.
+
+SELECT nombre, apellido1, apellido2 FROM empleado
+ORDER BY apellido1 ASC, nombre ASC;-- 15.Lista los apellidos y el nombre de todos los empleados, ordenados de forma alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su nombre.
+
+SELECT nombre, presupuesto FROM departamento
+ORDER BY presupuesto DESC
+LIMIT 3;-- 16.Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen mayor presupuesto.
+
+SELECT nombre, presupuesto FROM departamento
+ORDER BY presupuesto ASC
+LIMIT 3;-- 17.Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen menor presupuesto.
+
+SELECT nombre, gastos FROM departamento
+ORDER BY gastos DESC
+LIMIT 2;-- 18.Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen mayor gasto.
+
+SELECT nombre, gastos FROM departamento
+ORDER BY gastos ASC
+LIMIT 2;-- 19.Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen menor gasto.
+
+SELECT * FROM empleado
+LIMIT 2,5;-- 20.Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La tercera fila se debe incluir en la respuesta. La respuesta debe incluir todas las columnas de la tabla empleado
