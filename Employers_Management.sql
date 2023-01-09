a9a7b09281fa4bc970f95b4648b1cda2b1a2ee3d
Daniel Rodriguez <71362610+danielrodriguez007@users.noreply.github.com>
2023-01-09 11:39:11 -0500

    Update Employers_Management.sql

--- Employers_Management.sql
+++ Employers_Management.sql
@@ -155,3 +155,57 @@ WHERE id_departamento = 3;-- 35.Lista los nombres, apellidos y nif de los emplea
 
 SELECT nombre,apellido1,apellido2,nif,id_departamento FROM empleado
 WHERE id_departamento IN(2,4,5); -- 36.Lista los nombres, apellidos y nif de los empleados que trabajan en los departamentos 2, 4 o 5.
+
+-- 1.2.4 Consultas multitabla (Composición interna) 
+
+SELECT CONCAT(COALESCE(empleado.nombre, ' '),COALESCE(empleado.apellido1, ' '),COALESCE(empleado.apellido2, ' ')) AS nombreCompleto,
+departamento.nombre FROM empleado
+INNER JOIN departamento
+ON empleado.id_departamento = departamento.id; -- 1.Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
+
+SELECT empleado.nombre,empleado.apellido1,empleado.apellido2,
+departamento.nombre FROM empleado 
+INNER JOIN departamento
+ON empleado.id_departamento = departamento.id
+ORDER BY departamento.nombre ASC , empleado.apellido1 ASC; -- 2.Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno. Ordena el resultado, en primer lugar por el nombre del departamento (en orden alfabético) y en segundo lugar por los apellidos y el nombre de los empleados.
+
+SELECT departamento.id,departamento.nombre FROM departamento
+INNER JOIN empleado
+ON departamento.id = empleado.id_departamento; -- 3.Devuelve un listado con el identificador y el nombre del departamento, solamente de aquellos departamentos que tienen empleados.
+
+SELECT DISTINCT departamento.id, departamento.nombre, CONCAT(presupuesto-gastos) AS presupuestoActual FROM departamento
+INNER JOIN empleado 
+ON departamento.id = empleado.id_departamento;-- 4.Devuelve un listado con el identificador, el nombre del departamento y el valor del presupuesto actual del que dispone, solamente de aquellos departamentos que tienen empleados. El valor del presupuesto actual lo puede calcular restando al valor del presupuesto inicial (columna presupuesto) el valor de los gastos que ha generado (columna gastos).
+
+SELECT departamento.nombre FROM departamento
+INNER JOIN empleado 
+ON departamento.id = empleado.id_departamento 
+WHERE empleado.nif = '38382980M';  -- 5.Devuelve el nombre del departamento donde trabaja el empleado que tiene el nif 38382980M.
+
+SELECT departamento.nombre FROM departamento 
+INNER JOIN empleado 
+ON departamento.id = empleado.id_departamento 
+WHERE empleado.nombre LIKE '%pe';-- 6.Devuelve el nombre del departamento donde trabaja el empleado Pepe Ruiz Santana.
+
+SELECT * FROM empleado
+INNER JOIN departamento 
+ON empleado.id_departamento = departamento.id
+WHERE departamento.id = 5
+ORDER BY empleado.nombre; -- 7.Devuelve un listado con los datos de los empleados que trabajan en el departamento de I+D. Ordena el resultado alfabéticamente.
+
+SELECT * FROM empleado 
+INNER JOIN departamento 
+ON departamento.id = empleado.id_departamento 
+WHERE departamento.nombre IN('Sistemas', 'Contabilidad','I+D')
+ORDER BY empleado.nombre ASC;-- 8.Devuelve un listado con los datos de los empleados que trabajan en el departamento de Sistemas, Contabilidad o I+D. Ordena el resultado alfabéticament
+
+SELECT * FROM empleado 
+INNER JOIN departamento 
+ON departamento.id = empleado.id_departamento 
+WHERE departamento.presupuesto NOT BETWEEN 100000 AND 200000;-- 9.Devuelve una lista con el nombre de los empleados que tienen los departamentos que no tienen un presupuesto entre 100000 y 200000 euros.
+
+SELECT DISTINCT departamento.nombre FROM departamento 
+INNER JOIN empleado
+ON departamento.id = empleado.id_departamento 
+WHERE empleado.apellido2 IS NULL;-- 10.Devuelve un listado con el nombre de los departamentos donde existe algún empleado cuyo segundo apellido sea NULL. Tenga en cuenta que no debe mostrar nombres de departamentos que estén repetidos.
+
