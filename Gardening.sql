b5d215888b679dc01a01fd43a3ca6a90be8a770f
Daniel Rodriguez <71362610+danielrodriguez007@users.noreply.github.com>
2023-01-27 19:53:28 -0500

    Update Gardening.sql

--- Gardening.sql
+++ Gardening.sql
@@ -966,3 +966,40 @@ WHERE puesto LIKE 'Representante%';--5.Devuelve un listado con el nombre, apelli
 
 SELECT nombre_cliente FROM cliente
 WHERE pais = 'Spain';--6.Devuelve un listado con el nombre de los todos los clientes españoles.
+
+SELECT DISTINCT estado FROM pedido;--7.Devuelve un listado con los distintos estados por los que puede pasar un pedido.
+
+SELECT * FROM pedido;--8.Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
+SELECT DISTINCT codigo_cliente FROM pedido WHERE YEAR(fecha_pedido)='2008';--Utilizando la función YEAR de MySQL.
+SELECT DISTINCT codigo_cliente FROM pedido WHERE DATE_FORMAT(fecha_pedido,'%Y') = '2008';--Utilizando la función DATE_FORMAT de MySQL.
+SELECT DISTINCT codigo_cliente FROM pedido WHERE fecha_pedido BETWEEN '2008-01-01' AND '2008-12-31';--Sin utilizar ninguna de las funciones anteriores.
+
+SELECT codigo_pedido,codigo_cliente,fecha_esperada,fecha_entrega FROM pedido
+WHERE estado LIKE 'P%';--9.Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
+
+SELECT codigo_pedido,codigo_cliente,fecha_esperada,fecha_entrega,ADDDATE(fecha_entrega,2) AS diasDiferencia FROM pedido;--10.Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
+SELECT codigo_pedido,codigo_cliente,fecha_esperada,fecha_entrega FROM pedido
+WHERE CONCAT(ADDDATE(fecha_entrega,2)-fecha_esperada) = 0;--Utilizando la función ADDDATE de MySQL.
+SELECT codigo_pedido,codigo_cliente,fecha_esperada,fecha_entrega,DATEDIFF(fecha_esperada,fecha_entrega) AS diasDiferencia FROM pedido
+WHERE DATEDIFF(fecha_esperada,fecha_entrega) >=0;--Utilizando la función DATEDIFF de MySQL.
+--SI--¿Sería posible resolver esta consulta utilizando el operador de suma + o resta -
+
+SELECT * FROM pedido
+WHERE YEAR(fecha_pedido) ='2009' AND estado LIKE('R%');--11.Devuelve un listado de todos los pedidos que fueron rechazados en 2009.
+
+SELECT * FROM pedido
+WHERE MONTH(fecha_pedido) = '01' AND estado LIKE('E%');--12.Devuelve un listado de todos los pedidos que han sido entregados en el mes de enero de cualquier año.
+
+SELECT * FROM pago
+WHERE forma_pago LIKE 'PayPal%' AND YEAR(fecha_pago) = '2008'
+ORDER BY fecha_pago;--13.Devuelve un listado con todos los pagos que se realizaron en el año 2008 mediante Paypal. Ordene el resultado de mayor a menor.
+
+SELECT DISTINCT forma_pago FROM pago;--14.Devuelve un listado con todas las formas de pago que aparecen en la tabla pago. Tenga en cuenta que no deben aparecer formas de pago repetidas.
+
+SELECT * FROM producto
+WHERE gama LIKE 'Ornamentales%' AND cantidad_en_stock >100
+ORDER BY precio_venta DESC;--15.Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor preci
+
+SELECT * FROM cliente
+WHERE ciudad LIKE 'Madrid%' AND codigo_empleado_rep_ventas IN (11,30);--16.Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.
+SHOW TABLES;
