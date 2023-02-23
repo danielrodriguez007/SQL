94300905b2c8e09665873a08654333335622af0c
Daniel Rodriguez <71362610+danielrodriguez007@users.noreply.github.com>
2023-02-23 16:30:45 -0500

    Update LeetCode.sql

---
+++ LeetCode.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS Leet;
+CREATE DATABASE Leet CHARACTER SET utf8mb4;
+USE leet;
+
+Create table If Not Exists Person (id int, email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'a@b.com');
+insert into Person (id, email) values ('2', 'c@d.com');
+insert into Person (id, email) values ('3', 'a@b.com');
+
+
+select email as Email  from Person
+GROUP BY email
+HAVING count(email) > 1;
+
+
+Create table If Not Exists Customers (id int, name varchar(255));
+Create table If Not Exists Orders (id int, customerId int);
+Truncate table Customers;
+insert into Customers (id, name) values ('1', 'Joe');
+insert into Customers (id, name) values ('2', 'Henry');
+insert into Customers (id, name) values ('3', 'Sam');
+insert into Customers (id, name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (id, customerId) values ('1', '3');
+insert into Orders (id, customerId) values ('2', '1');
+
+select distinct (name) AS Customers from Customers
+where id NOT IN  (SELECT customerId FROM Orders) ;
