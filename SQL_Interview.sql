99e2a915fe58074174e20c7c394a644814590892
Daniel Rodriguez <71362610+danielrodriguez007@users.noreply.github.com>
2023-07-21 17:50:07 -0500

    Update SQL_Interview.sql

--- SQL_Interview.sql
+++ SQL_Interview.sql
@@ -22,6 +22,45 @@ VALUES
   (1003, 'Vikram Patel', '2023-07-09', 'Withdrawal', 2000);
 
 
+CREATE TABLE Trips(
+  ID INT,
+  CLIENTE_ID INT,
+  DRIVER_ID INT,
+  CITY_ID INT,
+  STATUS VARCHAR(20),
+  REQUEST_AT DATE
+);
+
+CREATE TABLE USERS(
+  USER_ID INT,
+  BANNED VARCHAR(3),
+  ROLE VARCHAR(10)
+);
+
+INSERT INTO TRIPS (ID,CLIENTE_ID,DRIVER_ID,CITY_ID,STATUS,REQUEST_AT)
+VALUES
+    (1, 1, 10, 1, 'completed', '2023-07-12'),
+    (2, 2, 11, 1, 'cancelled_by_driver', '2023-07-12'),
+    (3, 3, 12, 6, 'completed', '2023-07-12'),
+    (4, 4, 13, 6, 'cancelled_by_client', '2023-07-12'),
+    (5, 1, 10, 1, 'completed', '2023-07-13'),
+    (6, 2, 11, 6, 'completed', '2023-07-13'),
+    (7, 3, 12, 6, 'completed', '2023-07-13'),
+    (8, 2, 12, 12, 'completed', '2023-07-14'),
+    (9, 3, 10, 12, 'completed', '2023-07-14'),
+    (10, 4, 13, 12, 'cancelled_by_driver', '2023-07-14');
+
+INSERT INTO USERS (USER_ID, BANNED, ROLE)
+VALUES
+    (1, 'No', 'client'),
+    (2, 'Yes', 'client'),
+    (3, 'No', 'client'),
+    (4, 'No', 'client'),
+    (10, 'No', 'driver'),
+    (11, 'No', 'driver'),
+    (12, 'No', 'driver'),
+    (13, 'No', 'driver');    
+
 CREATE TEMPORARY TABLE TEMP_DEPOSIT(
     AccountNumber INT,
     AccountHolderName VARCHAR(255),
@@ -48,5 +87,34 @@ WHERE A.TRANSACTIONTYPE LIKE 'WITHDRAWAL'
 GROUP BY A.ACCOUNTNUMBER;
 
 ---------------------------------------------------------------------------------------------------------------------------------------
+WITH CANCELLED AS(
+ SELECT
+ ID,
+ REQUEST_AT,
+ COUNT(STATUS) CANCELLED
+ FROM TRIPS
+ WHERE STATUS LIKE 'CANCELLED%'
+ GROUP BY REQUEST_AT
+)
+SELECT
+B.REQUEST_AT,
+COUNT(B.STATUS) TOTAL_TRIPS,
+A.CANCELLED,
+CASE 
+  WHEN (A.CANCELLED/COUNT(B.STATUS))*100 IS NULL THEN 0
+  ELSE  (A.CANCELLED/COUNT(B.STATUS))*100
+END RATE
+FROM CANCELLED A
+RIGHT JOIN TRIPS B
+ON 
+A.REQUEST_AT = B.REQUEST_AT
+GROUP BY B.REQUEST_AT;
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------
 USE interview;
-SELECT * FROM BANKTRANSACTIONS;
+SHOW TABLES;
+SELECT * FROM USERS;
+SELECT * FROM TRIPS;
